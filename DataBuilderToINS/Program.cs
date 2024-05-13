d571caf751c9c75c45dd98c1b327cc428b2273d4
LofasekU18 <lstrnad13@gmail.com>
2024-05-13 17:53:37 +0200

    Start

---
+++ DataBuilderToINS/Program.cs
@@ -0,0 +1,9 @@
+﻿/*TODO:
+1. SOAP request to isir
+2. Parse answer from XML to object - dluznik
+3. Connect to db via EF
+4. Parse data from db to object - celkova castka(7865/6655) - vymozna castka, usn/pov, 
+
+
+*/
+Console.WriteLine("Hello, World!");
