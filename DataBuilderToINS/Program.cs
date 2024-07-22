eda34456062b8069646408b25c5fb30eb46c114a
LofasekU18 <lstrnad13@gmail.com>
2024-07-23 00:23:49 +0200

    Rename some stuffs

--- DataBuilderToINS/Program.cs
+++ DataBuilderToINS/Program.cs
@@ -10,7 +10,7 @@ using InsXml;
 - save to file 1/3
 - SQL for GetRowFromDatabase, SELECT SUM of UP, usn/pov(some logic to use right document and right text for 6655 or 7865) 
 - Some menu
-- test if i need I/O method to db in async
+- XMLSaver if i need I/O method to db in async
 
 */
 //"01881485"
@@ -19,11 +19,11 @@ using InsXml;
 
 // var listResultFromDb = OleConnect.GetRowFromDatabase<List<string>>("SELECT TOP 1 * FROM Entry;"); // Querry SELECT Adresy.RC,Adresy.IC FROM Povinni INNER JOIN ADRESY ON Povinni.[Nazov subjektu]=Adresy.[Nazov subjektu] WHERE Povinni.HlavaI=true AND Povinni.HlavaII=true AND Povinni.HlavaIII=true AND Povinni.[Ex cislo] LIKE {inputExko};
 string inputExko = null;
-while (inputExko == null)
-{
-    Console.WriteLine("Zadej exko");
-    inputExko = Console.ReadLine();
-}
+// while (inputExko == null)
+// {
+//     Console.WriteLine("Zadej exko");
+//     inputExko = Console.ReadLine();
+// }
 
 // PrimaryData primaryData = OleConnect.GetRowFromDatabase<PrimaryData>("SELECT TOP 1 * FROM Entry;");
 PrimaryData primaryData = new("750720/0316", "01881485");
@@ -40,13 +40,13 @@ if (primaryData.RC is not null)
 {
     DataIsirRC resultFromIsir2 = ParseXmlToData.CreateDataRC(await SearchSoap.SoapSearchingRC(primaryData.RC));
     if (resultFromIsir2 != null)
-        File.WriteAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "prihlaska.xml"), Test.CreateXmlFo(resultFromIsir2, OleConnect.GetRowFromDatabase<DataMsAccess>("SELECT TOP 1 * FROM Entry;")));
+        File.WriteAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "prihlaska.xml"), XMLSaver.CreateXmlFo(resultFromIsir2, OleConnect.GetRowFromDatabase<DataMsAccess>("SELECT TOP 1 * FROM Entry;")));
 }
 else
 {
     DataIsirIC resultFromIsir = ParseXmlToData.CreateDataIC(await SearchSoap.SoapSearchingIC(primaryData.IC));
     if (resultFromIsir != null)
-        File.WriteAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "prihlaska.xml"), Test.CreateXmlPo(resultFromIsir, OleConnect.GetRowFromDatabase<DataMsAccess>("SELECT TOP 1 * FROM Entry;")));
+        File.WriteAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "prihlaska.xml"), XMLSaver.CreateXmlPo(resultFromIsir, OleConnect.GetRowFromDatabase<DataMsAccess>("SELECT TOP 1 * FROM Entry;")));
 }
 
 record PrimaryData(string RC, string IC);
@@ -68,13 +68,13 @@ switch (IntendedID.Intended(a))
     case 1:
         DataIsirRC resultFromIsir = ParseXmlToData.CreateDataRC(await SearchSoap.SoapSearchingRC(a));
         if (resultFromIsir != null)
-            File.WriteAllText("prihlaska.xml", Test.CreateXmlFo(resultFromIsir, OleConnect.GetRowFromDatabase<DataMsAccess>("SELECT TOP 1 * FROM Entry;")));
+            File.WriteAllText("prihlaska.xml", XMLSaver.CreateXmlFo(resultFromIsir, OleConnect.GetRowFromDatabase<DataMsAccess>("SELECT TOP 1 * FROM Entry;")));
         break;
 
     case 2:
         DataIsirIC resultFromIsir2 = ParseXmlToData.CreateDataIC(await SearchSoap.SoapSearchingIC(a));
         if (resultFromIsir2 != null)
-            File.WriteAllText("prihlaska.xml", Test.CreateXmlPo(resultFromIsir2, OleConnect.GetRowFromDatabase<DataMsAccess>("SELECT TOP 1 * FROM Entry;")));
+            File.WriteAllText("prihlaska.xml", XMLSaver.CreateXmlPo(resultFromIsir2, OleConnect.GetRowFromDatabase<DataMsAccess>("SELECT TOP 1 * FROM Entry;")));
         break;
     case 0:
         System.Console.WriteLine("Chyba");
